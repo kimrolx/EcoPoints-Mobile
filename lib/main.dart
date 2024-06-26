1270e808d00379d6715fb02dba7d6a6792345f8d
Kim Berame <kimroland.berame.21@usjr.edu.ph>
2024-06-26 12:43:11 +0800

    added firebase and configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'src/controllers/auth_controller.dart';
 import 'src/routes/router.dart';
 import 'src/shared/utils/local_storage_util.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await LocalStorage.init();
   AuthController.initialize();
   GlobalRouter.initialize();
