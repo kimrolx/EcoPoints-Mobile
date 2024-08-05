f27d8ed84ede43f8c3cd279ff3faeedf88ed2ea3
Kim Berame <kimroland.berame.21@usjr.edu.ph>
2024-08-05 23:05:46 +0800

    improved UI for reward details screen and added amount picker

--- lib/src/features/screens/reward-details-screen/reward_details_screen.dart
+++ lib/src/features/screens/reward-details-screen/reward_details_screen.dart
@@ -1,16 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
+import 'package:get_it/get_it.dart';
 
-import '../../../components/buttons/custom_elevated_button.dart';
 import '../../../components/constants/colors/ecopoints_colors.dart';
-import '../../../components/constants/text_style/ecopoints_themes.dart';
 import '../../../models/reward_model.dart';
 import '../../../models/transaction_model.dart';
-import '../../../shared/utils/date_formatter_util.dart';
+import '../../../shared/services/user_profile_service.dart';
 import 'widgets/confirm_claim_dialog.dart';
+import 'widgets/insufficient_points_dialog.dart';
 import 'widgets/picture_stack.dart';
-import 'widgets/text_details.dart';
+import 'widgets/text_details_container.dart';
 
 class RewardDetailsScreen extends StatefulWidget {
   final RewardModel reward;
@@ -26,6 +25,10 @@ class RewardDetailsScreen extends StatefulWidget {
 }
 
 class _RewardDetailsScreenState extends State<RewardDetailsScreen> {
+  final UserProfileService _userProfileService =
+      GetIt.instance<UserProfileService>();
+
+  late double totalPrice;
   int totalAmount = 1;
   String userName = '';
 
@@ -33,6 +36,8 @@ class _RewardDetailsScreenState extends State<RewardDetailsScreen> {
   void initState() {
     super.initState();
     _fetchUserName();
+    _userProfileService.loadUserProfile();
+    totalPrice = widget.reward.requiredPoint;
   }
 
   void _fetchUserName() async {
@@ -52,68 +57,66 @@ class _RewardDetailsScreenState extends State<RewardDetailsScreen> {
     );
   }
 
+  void _showInsuffientPointsDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => const InsufficientPointsDialogRewardDetailsScreen(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
-    String formattedExpiryDate =
-        DateFormatterUtil.formatDateWithoutTime(widget.reward.expiryDate);
-
     return Scaffold(
       backgroundColor: EcoPointsColors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
-          PictureStackRewardDetailsScreen(reward: widget.reward),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: width * 0.03, vertical: height * 0.01),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Expanded(
-                      child: TextDetailsRewardDetailsScreen(
-                          reward: widget.reward)),
-                  Text(
-                    "Expires: $formattedExpiryDate",
-                    style: EcoPointsTextStyles.blackTextStyle(
-                        size: width * 0.035, weight: FontWeight.normal),
-                  ),
-                  Gap(height * 0.03),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: CustomElevatedButton(
-                      borderRadius: 7,
-                      width: width * 0.8,
-                      backgroundColor: EcoPointsColors.darkGreen,
-                      onPressed: () {
-                        final transaction = TransactionModel(
-                          reward: widget.reward,
-                          quantity: totalAmount,
-                          totalPrice: widget.reward.requiredPoint * totalAmount,
-                          timeCreated: DateTime.now(),
-                          referenceId:
-                              ReferenceIdGenerator.generateReferenceId(),
-                          userName: userName,
-                        );
-                        _showConfirmClaimDialog(context, transaction);
-                      },
-                      child: Text(
-                        "Claim Reward",
-                        style: EcoPointsTextStyles.whiteTextStyle(
-                            size: width * 0.04, weight: FontWeight.w600),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+          SizedBox(width: width, height: height),
+          Container(
+            height: height * 0.55,
+            color: EcoPointsColors.red,
+            child: PictureStackRewardDetailsScreen(
+              reward: widget.reward,
+            ),
+          ),
+          Positioned(
+            top: height * 0.5,
+            left: 0,
+            right: 0,
+            child: TextDetailsContainerRewardDetailsScreen(
+              reward: widget.reward,
+              totalAmount: totalAmount,
+              onAmountChanged: (newAmount) {
+                setState(() {
+                  totalAmount = newAmount;
+                  totalPrice = widget.reward.requiredPoint * totalAmount;
+                });
+              },
+              onClaimPressed: onClaimPressed,
             ),
           ),
         ],
       ),
     );
+  }
+
+  onClaimPressed() {
+    final transaction = TransactionModel(
+      reward: widget.reward,
+      quantity: totalAmount,
+      totalPrice: totalPrice,
+      timeCreated: DateTime.now(),
+      referenceId: ReferenceIdGenerator.generateReferenceId(),
+      userName: userName,
+    );
+
+    double userPoints = _userProfileService.userProfile?.points ?? 0.00;
+    if (userPoints >= totalPrice) {
+      _showConfirmClaimDialog(context, transaction);
+    } else {
+      _showInsuffientPointsDialog(context);
+    }
   }
 }
