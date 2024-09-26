4a64bfb735d8e4a85045037147146afa850f3690
Kim Berame <kimroland.berame.21@usjr.edu.ph>
2024-09-26 22:09:55 +0800

    increased widths for clickable texts, changed inkwell to gesturedetector for clickable texts

--- lib/src/features/screens/login-screen/widgets/forgot_password.dart
+++ lib/src/features/screens/login-screen/widgets/forgot_password.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../../components/constants/colors/ecopoints_colors.dart';
+import '../../../../components/constants/text_style/ecopoints_themes.dart';
 import '../../../../components/misc/custom_circular_progress_indicator.dart';
 
 class ForgotPasswordLoginScreen extends StatelessWidget {
@@ -12,22 +13,25 @@ class ForgotPasswordLoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    return TextButton(
-      onPressed: onPressed,
-      style: TextButton.styleFrom(
-        padding: EdgeInsets.zero,
-        minimumSize: const Size(0, 0),
-        textStyle: TextStyle(
-          fontSize: width * 0.035,
-          fontWeight: FontWeight.normal,
+    double height = MediaQuery.of(context).size.height;
+
+    return SizedBox(
+      width: width * 0.5,
+      height: height * 0.04,
+      child: Center(
+        child: GestureDetector(
+          onTap: onPressed,
+          child: isLoading
+              ? const CustomCircularProgressIndicator(
+                  backgroundColor: EcoPointsColors.white,
+                  width: 22.5,
+                  height: 22.5,
+                )
+              : Text("Forgot your password?",
+                  style: EcoPointsTextStyles.darkGreenTextStyle(
+                      size: width * 0.035, weight: FontWeight.normal)),
         ),
       ),
-      child: isLoading
-          ? const CustomCircularProgressIndicator(
-              backgroundColor: EcoPointsColors.white, width: 22.5, height: 22.5)
-          : const Text(
-              "Forgot your password?",
-            ),
     );
   }
 }
